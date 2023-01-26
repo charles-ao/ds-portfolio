9ca6b34703a0b6ff437772b968b63d8af4838015
Adedunmola <adeiyecharles@gmail.com>
2023-01-26 12:23:55 +0000

    Under construction

---
+++ src/Sections/Construction/Contruction.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Construction = () => {
+
+    return (
+        <div className="construction">
+            <div className="construction-container">
+                <h1 className="construction-text">Under Maintenance</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Construction
