a38bbc2c9d03900a4597c9f9de6e06efa7f2f53d
Adedunmola <adeiyecharles@gmail.com>
2023-01-26 21:40:54 +0000

    troubleshooting routing issue 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+// import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
+// import Home from "./Components/Home/Home";
 import Navbar from './Components/Navbar/Navbar';
 import { useGlobalContext } from "./Context";
+import About from "./Sections/About/About";
+import Contact from "./Sections/Contact/Contact";
+import Herohead from "./Sections/Herohead/Herohead";
+import Portfolio from "./Sections/Portfolio/Portfolio";
 
 const  App = ()  => {
  const {navStatus} = useGlobalContext()
@@ -12,11 +16,10 @@ const  App = ()  => {
   return (
     <div>
       <Navbar showNav={navStatus}/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Herohead/>
+      <About/>
+      <Portfolio/>
+      <Contact />
       <Footer/>
     </div>
   );
