2fb9113d55f2208b33b90f30f00e82209a81952d
Adedunmola <adeiyecharles@gmail.com>
2023-02-01 10:11:58 +0000

    update contact

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -16,8 +16,8 @@ const Contact = () => {
                 </div>
                 <div className="contact-text">
                     <p>
-                    Although I’m not currently looking for any new opportunities,
-                    my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+                    I’m currently looking for any new opportunities.
+                    My inbox is always open, whether you have a question or just want to say hi, I’ll try my best to get back to you!
                     </p>
                 </div>
                 <div className="contact-social-media-icons">
